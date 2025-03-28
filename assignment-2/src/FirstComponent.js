2e7db8b941c7a26a2ec4682670f77e6f1ae8d2ae
Abdul Rehman Zubairi <associatewithabdulrehman@gmail.com>
2025-03-28 10:03:44 +0500

    commit

---
+++ assignment-2/src/FirstComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function FirstComponent() {
+  console.log("FirstComponent is rendering"); // Debugging log
+  return <h2>This is the First Component</h2>;
+}
+
+export default FirstComponent;
---
+++ assignment-2/src/SecondComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function SecondComponent() {
+  console.log("SecondComponent is rendering"); // Debugging log
+  return <h2>This is the Second Component</h2>;
+}
+
+export default SecondComponent;
