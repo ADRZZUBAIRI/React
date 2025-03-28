2dcd429fdd9bc2af30edb1214c4762c026bda52c
Abdul Rehman Zubairi <associatewithabdulrehman@gmail.com>
2025-03-28 10:52:43 +0500

    Revert "commit"
    This reverts commit 2e7db8b941c7a26a2ec4682670f77e6f1ae8d2ae.

--- assignment-2/src/FirstComponent.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function FirstComponent() {
-  console.log("FirstComponent is rendering"); // Debugging log
-  return <h2>This is the First Component</h2>;
-}
-
-export default FirstComponent;
--- assignment-2/src/SecondComponent.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function SecondComponent() {
-  console.log("SecondComponent is rendering"); // Debugging log
-  return <h2>This is the Second Component</h2>;
-}
-
-export default SecondComponent;
